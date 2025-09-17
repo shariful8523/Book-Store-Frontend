b324d1cef40a999588ad2d17981a8dd64abde270
shariful8523 <mdsharifulislam8523@gmail.com>
2025-09-17 22:55:53 +0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home';
+import CartPage from './Pages/CartPage';
+import AboutPage from './Pages/AboutPage';
+import BookPage from './Pages/BookPage';
+import ContactPage from './Pages/ContactPage';
+import Login from './Components/Login';
+import Signup from './Components/Signup';
 
 const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Home/>}  />
+      <Route path='/cart' element={<CartPage/>} />
+      <Route path='/about' element={<AboutPage/>} />
+      <Route path='/books' element={<BookPage/>} />
+      <Route path='/contact' element={<ContactPage/>} />
+
+      <Route path='/login' element={<Login/>} />
+      <Route path='/signup' element={<Signup/>} />
     </Routes>
   );
 };
